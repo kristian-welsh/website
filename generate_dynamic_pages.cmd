5ad99dcfa0a6a7e996b095049cd2884cc5d90867
Kristian Welsh <kriswelsh@gmail.com>
2015-02-21 11:09:49 +0000

    Created a little script to generate all current game pages

---
+++ generate_dynamic_pages.cmd
@@ -0,0 +1,5 @@
+@ echo off
+ruby GenerateGamePage.rb "My Little Pony Fan Game" "An RPG based in the My Little Pony universe." 550 400
+ruby GenerateGamePage.rb "Chess" "A little chess game." 314 314
+ruby GenerateGamePage.rb "Rafter Sleuth" "A game about a bat finding lost property in a train station." 800 600
+ruby GenerateGamePage.rb "Type And Run" "A mix between a spelling game and an endless runner." 800 600
